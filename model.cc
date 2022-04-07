629957443d82a2c9f947048bbd92e1f860ecd96d
Gregoire Chomette <greg.chomette@gmail.com>
2022-04-07 16:24:32 -0700

    Moved the model class to a separate file (header and implementation)

---
+++ model.cc
@@ -0,0 +1,193 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <math.h>
+#include <stdio.h>
+#include <ctime>
+#include <time.h>
+#include <algorithm>
+#include <vector>
+
+#include "model.h"
+
+
+Model::Model(const std::string folderName):
+        _folderName(folderName)
+        {
+            readWeightsFile(), 
+            readingScalingParameters();
+        }
+
+
+void Model::readWeightsFile(){
+
+    // Open the file and declare reading variables
+    std::fstream file (_folderName + "weights.csv", std::ios::in);
+    std::vector<double> row;
+    std::string line, word;
+
+    // Read the file and store data
+    if(file.is_open()){
+        while(getline(file, line)){
+            row.clear();
+            std::stringstream str(line);
+            while(getline(str, word, ',')){
+                row.push_back(std::stod(word));
+            }
+            _weights.push_back(row);
+        }
+    }
+    else{
+        std::cout << "Error: Cannot open weights file "  << std::endl;
+    }
+    return;
+}
+
+
+void Model::readingScalingParameters(){
+
+    // Open the file and declare reading variables
+    std::string scalingFileName = _folderName + "scaling_parameters.csv";
+    std::fstream file (scalingFileName, std::ios::in);
+    std::vector<std::string> row;
+    std::string line, word;
+
+    // Read the file and store data
+    if(file.is_open()){
+        while(getline(file, line)){
+            row.clear();
+            std::stringstream str(line);
+            while(getline(str, word, ',')){
+                row.push_back(word);
+            }
+            _scalingParameters.push_back(row);
+        }
+    }
+    else{
+        std::cout << "Error: Cannot open parameter scaling parameter file "  << std::endl;
+    }
+    return;
+}
+
+    
+std::vector<double> Model::normalizeInputs(std::vector<float> data){
+    std::vector<double> normalizedInputs(9);
+    for (int i=0; i<9; i++){
+        double mean = std::stof(_scalingParameters[1][i+1]);
+        double std = std::stof(_scalingParameters[2][i+1]);
+        normalizedInputs[i] = (data[i] - mean)/ (std);
+    }
+    return normalizedInputs;
+}
+
+
+double Model::rescaleOutput(double damageNormalized){
+    double mean = std::stof(_scalingParameters[1][10]);
+    double std = std::stof(_scalingParameters[2][10]);
+    return (damageNormalized * std) + mean;
+}
+
+        
+std::vector<double> Model::matrixTransposedVectorMultiplication(std::vector<double> A, std::vector<double> x, size_t mA, size_t nA){
+    std::vector<double> output(nA, 0.0);
+    for (int j=0; j<nA; j++){
+        for (int i=0; i<mA; i++){
+            output[j] += A[i * nA + j] * x[i];
+        }
+    }
+    return output;
+}
+
+
+std::vector<double> Model::matrixVectorMultiplication(std::vector<double> A, std::vector<double> x, size_t mA, size_t nA){
+    std::vector<double> output(mA, 0.0);
+    for (int i=0; i<mA; i++){
+        for (int j=0; j<nA; j++){
+            output[i] += A[i * nA + j] * x[j];
+        }
+    }
+    return output;
+}
+
+
+std::vector<double> Model::vectorsElementwiseMultiplication(std::vector<double> a, std::vector<double> b){
+    std::vector<double> output(a.size(), 0.0);
+    for (int i=0; i<a.size(); i++){
+        output[i] = a[i] * b[i];
+    }
+    return output;
+}
+
+
+std::vector<double> Model::vectorsAddition(std::vector<double> a, std::vector<double> b){
+    for (int i=0; i<a.size(); i++){
+        a[i] += b[i];
+    }
+    return a;
+}
+
+
+std::vector<double> Model::rectifiedLinearUnit(std::vector<double> a){
+    for (int i=0; i<a.size(); i++){
+        if(a[i] < 0){
+            a[i] = 0;
+        }
+    }
+    return a;
+}
+
+
+std::vector<double> Model::derRectifiedLinearUnit(std::vector<double> a){
+    for (int i=0; i<a.size(); i++){
+        if (a[i] < 0){
+            a[i] = 0;
+        }else{
+            a[i] = 1;
+        }
+    }
+    return a;
+}
+
+
+std::vector<double> Model::forwardPropagation(std::vector<double> input){
+
+    _z1 = vectorsAddition(matrixTransposedVectorMultiplication(_weights[0], input, _layers[0], _layers[1]), _weights[1]);
+    _a1 = rectifiedLinearUnit(_z1);
+    _z2 = vectorsAddition(matrixTransposedVectorMultiplication(_weights[2], _a1, _layers[1], _layers[2]), _weights[3]);
+    _a2 = rectifiedLinearUnit(_z2);
+    _z3 = vectorsAddition(matrixTransposedVectorMultiplication(_weights[4], _a2, _layers[2], _layers[3]), _weights[5]);
+    _a3 = rectifiedLinearUnit(_z3);
+    _z4 = vectorsAddition(matrixTransposedVectorMultiplication(_weights[6], _a3, _layers[3], _layers[4]), _weights[7]);
+    return _z4;
+}
+
+double Model::evaluateOutput(std::vector<float> data){
+
+    // Normalize the input data
+    std::vector<double> normalizedInputs = normalizeInputs(data);
+    // Call fhe forward propagation method
+    double damageNormalized = forwardPropagation(normalizedInputs)[0];
+    // Rescale and return the output data
+    return rescaleOutput(damageNormalized);
+}
+
+
+std::vector<double> Model::backPropagation(){
+    _dZ4dZ3 = vectorsElementwiseMultiplication(derRectifiedLinearUnit(_z3), _weights[6]);
+    _dZ4dA2 = matrixVectorMultiplication(_weights[4], _dZ4dZ3, _layers[2], _layers[3]);
+    _dZ4dZ2 = vectorsElementwiseMultiplication(derRectifiedLinearUnit(_z2), _dZ4dA2);
+    _dZ4dA1 = matrixVectorMultiplication(_weights[2], _dZ4dZ2, _layers[1], _layers[2]);
+    _dZ4dZ1 = vectorsElementwiseMultiplication(derRectifiedLinearUnit(_z1), _dZ4dA1);
+    _dZ4dA0 = matrixVectorMultiplication(_weights[0], _dZ4dZ1, _layers[0], _layers[1]);
+    return _dZ4dA0;
+}
+
+
+std::vector <double> Model::globalDerivatives(std::vector<double> nnDerivatives, double pop, double rad){
+    std::vector<double> derivatives(9);
+    for (int i=0; i<derivatives.size(); i++){
+        derivatives[i] = 2 * pop * std::stof(_scalingParameters[2][i+1]) * nnDerivatives[i] / (std::stof(_scalingParameters[2][10]) * rad);
+    }
+    return derivatives;
+}
---
+++ model.h
@@ -0,0 +1,171 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <math.h>
+#include <stdio.h>
+#include <ctime>
+#include <time.h>
+#include <algorithm>
+#include <vector>
+
+// #include "input.h"
+
+
+class Model{
+
+    public:
+        /**
+         * @brief Construct the Model object
+         * 
+         */
+
+        Model(const std::string folderName);
+
+        /**
+         * @brief Method to read the weights and store the three layers
+         * 
+         */
+        void readWeightsFile();
+
+        /**
+         * @brief Method to read and store the scaling parameters
+         * 
+         */
+
+        void readingScalingParameters();
+
+        /**
+         * @brief Method to normalize the input parameters before feeding the neural network
+         * 
+         * @param data Input object containing real-scale asteroid properties and trajectories
+         * @return std::vector<float> vector containing normalized asteroid properties and trajectories
+         */
+
+        std::vector<double> normalizeInputs(std::vector<float> data);
+
+        /**
+         * @brief Method to rescale the neural network output to real-life casualties
+         * 
+         * @param damageNormalized output of the neural network
+         * @return float real-life casualty radius from the asteroid(s) impacts(s)
+         */
+
+        double rescaleOutput(double damageNormalized);
+
+
+        /**
+         * @brief Method to compute A^Tx = b
+         * 
+         * @param A matrix stored in a vector of size (mA x nA), row-major-ordered
+         * @param x vector of size (mB x 1)
+         * @param mA number of rows of matrix A
+         * @param nA number of columns of matrix A
+         * @return A^Tx
+         */
+        
+        std::vector<double> matrixTransposedVectorMultiplication(std::vector<double> A, std::vector<double> x, size_t mA, size_t nA);
+
+        /**
+         * @brief Method to compute Ax = b
+         * 
+         * @param A matrix stored in a vector of size (mA x nA), row-major-ordered
+         * @param x vector of size (mB x 1)
+         * @param mA number of rows of matrix A
+         * @param nA number of columns of matrix A
+         * @return Ax
+         */
+
+        std::vector<double> matrixVectorMultiplication(std::vector<double> A, std::vector<double> x, size_t mA, size_t nA);
+
+        /**
+         * @brief Method for elementwise multiplication between two vectors
+         * 
+         * @param a First vector
+         * @param b Second vector
+         * @return vector containing a[i] * b[i]
+         */
+
+        std::vector<double> vectorsElementwiseMultiplication(std::vector<double> a, std::vector<double> b);
+
+        /**
+         * @brief Method for elementwise addition between two  vectors
+         * 
+         * @param a First vector
+         * @param b Second vector
+         * @return vector containing a[i] + b[i]
+         */
+
+        std::vector<double> vectorsAddition(std::vector<double> a, std::vector<double> b);
+
+        /**
+         * @brief Method to compute the elementwise max(a,0)
+         * 
+         * @param a Vector
+         * @return vector containing max(a[i],0)
+         */
+
+        std::vector<double> rectifiedLinearUnit(std::vector<double> a);
+
+        /**
+         * @brief Method to compute the elementwise derivative of max(a,0)
+         * 
+         * @param a Vector
+         * @return vector containing the derivative of max(a[i],0)
+         */
+
+        std::vector<double> derRectifiedLinearUnit(std::vector<double> a);
+
+        /**
+         * @brief Method to infer the output of a neural network containing 4 layers
+         * 
+         * @param input vector fed to the neural network
+         * @return output vector of the neural network
+         */
+
+        std::vector<double> forwardPropagation(std::vector<double> input);
+
+
+        double evaluateOutput(std::vector<float> data);
+
+        /**
+         * @brief Method to compute the derivative of the ouput of a neural network w.r.t the inputs
+         * 
+         * @return derivatives of the outputs with respect to the inputs
+         */
+
+        std::vector<double> backPropagation();
+
+        /**
+         * @brief Method to compute the derivatives of full-scale output w.r.t. full-scale inputs
+         * 
+         * @param nnDerivatives derivates of the neural network (normalized inputs and outputs)
+         * @param pop number of people affected by the asteroid scenario
+         * @param rad radius of damage
+         * @return std::vector <double> 
+         */
+
+        std::vector <double> globalDerivatives(std::vector<double> nnDerivatives, double pop, double rad);
+
+        /**
+         * @brief Attributes of the model
+         * 
+         */
+
+        // Folder path for the neural network model
+        const std::string _folderName;
+        // Number of units of the different layers (including input and output)
+        std::vector<int> _layers = {9,64,128,256,1};
+        // Intermediate pre-activation results of the neural network
+        std::vector<double> _z1, _z2, _z3, _z4;
+        // Intermediate post-activation results of the neural network
+        std::vector<double> _a1, _a2, _a3;
+        // Intermediate derivatives 
+        std::vector<double> _dZ4dZ3, _dZ4dA2, _dZ4dZ2, _dZ4dA1, _dZ4dZ1, _dZ4dA0;
+        // Vector of vectors containing all the weights of the neural network
+        std::vector<std::vector<double> > _weights;
+        // Trajectory and material parameters
+        std::vector<float> _scenarioParameters;
+        // Scaling parameters for normalization of inputs/outputs
+        std::vector<std::vector<std::string> > _scalingParameters;
+};
