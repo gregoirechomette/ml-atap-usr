c97b5016f9113906ee768310d0b66271a49cbe9e
Gregoire Chomette <greg.chomette@gmail.com>
2022-04-20 09:15:59 -0700

    ml-predictions.cc: Removed the mention of the input file

--- ml-predictions.cc
+++ ml-predictions.cc
@@ -19,79 +19,6 @@ const double earthRadius = 6.371e6;
 const double worldPop = 7.93 * pow(10,9);
 
 
-class Input {
-
-    public:
-
-        /**
-         * @brief Constructor of a new Input object
-         * 
-         * @param fileName path and name of the .dat file to be read
-         */
-
-        Input(const std::string fileName): _fileName(fileName) {
-            readFile();
-        }
-
-    private:
-
-        /**
-         * @brief Method to read and store the dat file containing scenario parameters
-         * 
-         */
-        
-        void readFile() {
-
-            // Prepare the vector   
-            std::vector<float> scenarioParameters(9);
-
-            // Try to open filename
-            std::ifstream source(_fileName.data());
-            if (source.fail()) {
-            std::cout << "Error: Cannot open parameter input file: " << _fileName.data() 
-                        << std::endl;
-            return;
-            }
-            // actually read file
-            source >> scenarioParameters[0]; 
-            source.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-            source >> scenarioParameters[1];    
-            source.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-            source >> scenarioParameters[2];
-            source.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-            source >> scenarioParameters[3];
-            source.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-            source >> scenarioParameters[4]; 
-            source.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-            source >> scenarioParameters[5];
-            source.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-            source >> scenarioParameters[6];
-            source.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-            source >> scenarioParameters[7]; 
-            source.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-            source >> scenarioParameters[8]; 
-            source.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-            source >> _latitude; 
-            source.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-            source >> _longitude; 
-            source.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-            source.close();
-
-            _scenarioParameters = scenarioParameters;
-
-            // all done
-            return;
-        }
-
-    public:
-        // Declare attributes
-        double _latitude, _longitude;
-        const std::string _fileName;
-        std::vector<float> _scenarioParameters;
-};
-
-
-
 int main() {
 
     // Material parameters 
@@ -111,12 +38,8 @@ int main() {
     float angle = 45;
     float azimuth = 180;
 
+    // Wrap all the parameters in one vector
     std::vector<float> dataInp = {diameter, density, strength, alpha, velocity, angle, azimuth, lumEff, ablation};
-    
-
-    // Instantiate the input objects with the scenario properties
-    const std::string propertiesFile = "../input.dat";
-    Input data(propertiesFile);
 
     // Instantiate the population grid vector
     const std::string popGridFile = "../pop-grids/popgrid-2020-2pt5arcmin.bin";
